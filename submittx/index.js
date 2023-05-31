bdf077acf2633c766a99d37da0513bc62a840d5f
ray-robot <hello@raynetwork.io>
2023-05-31 07:00:54 +0000

    Fix request type

--- submittx/index.js
+++ submittx/index.js
@@ -6,7 +6,7 @@ const PORT = 8700
 const HOST = '0.0.0.0'
 
 const app = express()
-app.use(express.text())
+app.use(express.text({type:"*/*"}))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
