791d6befad0920de647906fbe7011951911e3089
xray-robot <robot@xray.app>
2024-05-10 21:05:11 +0000

    Submittx application/cbor content type

--- submittx/index.js
+++ submittx/index.js
@@ -8,7 +8,24 @@ const OGMIOS_PORT = process.env.OGMIOS_PORT
 const app = express()
 const router = express.Router()
 
-app.use(express.text())
+app.use((req, res, next) => {
+    if (req.headers['content-type'] === 'application/cbor') {
+      let rawData = ''
+      req.setEncoding('utf8')
+
+      req.on('data', (chunk) => {
+        rawData += chunk
+      })
+
+      req.on('end', () => {
+        req.body = rawData
+        next()
+      })
+    } else {
+      next()
+    }
+})
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
@@ -19,21 +36,26 @@ app.use((req, res, next) => {
 router.post("/", async (req, res) => {
   const tx = req.body
   try {
-    const response = await fetch(`http://${OGMIOS_HOST}:${OGMIOS_PORT}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        "jsonrpc": "2.0",
-        "method": "submitTransaction",
-        "params": {
-          "transaction": {
-            "cbor": tx,
-          },
-        },
-      }),
-    })
 
-    const result = await response.json()
-    res.status(response.status).send(result)
+    if (req.headers['content-type'] === 'application/cbor') {
+      const response = await fetch(`http://${OGMIOS_HOST}:${OGMIOS_PORT}`, {
+        method: 'POST',
+        body: JSON.stringify({
+          "jsonrpc": "2.0",
+          "method": "submitTransaction",
+          "params": {
+            "transaction": {
+              "cbor": tx.slice(1, -1),
+            },
+          },
+        }),
+      })
+
+      const result = await response.json()
+      res.status(response.status).send(result)
+    } else {
+      res.send(415, "Unsupported Media Type")
+    }
   }
   catch (error) {
     console.log("Submittx ::", new Date().toISOString(), "::", JSON.stringify(error))
